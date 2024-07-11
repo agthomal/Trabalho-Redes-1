ec851423a76cba55837a51cc55e27c74e4529771
agthomal <agthomal@gmail.com>
2024-07-11 14:08:51 -0300

    Mudanca leve na estrutura dos arquivos

--- Instancias separadas/mensagem.h
+++ Instancias separadas/mensagem.h
@@ -1,6 +1,14 @@
 #ifndef MENSAGEM
 #define MENSAGEM
 
+int cria_raw_socket(char* nome_interface_rede);
+
+long long timestamp();
+
+int protocolo_e_valido(char* buffer, int tamanho_buffer);
+
+int recebe_mensagem(int soquete, int timeoutMillis, char* buffer, int tamanho_buffer);
+
 void prepara_mensagem(unsigned char msg[], unsigned char marcador, unsigned char tamanho, unsigned char sequencia, unsigned char tipo);
 
 unsigned char obtem_tamanho(unsigned char msg[]);
