63faae519471dc96b1288583dd437ad847f78b7b
Rafael Santos <rafa1405rafael@gmail.com>
2024-07-13 18:03:27 -0300

    .

--- t2/jogo.c
+++ t2/jogo.c
@@ -8,120 +8,84 @@
 #include <arpa/inet.h>
 
 #define PORT 33000
-#define IP_ADRESS "192.168.0.166"
-#define BUFFER_SIZE 1024
-
-void send_msg (char *message) {
-    int sockdg;
-    struct sockaddr_in server_addr;
-    
-    if ((sockdg = socket (AF_INET, SOCK_DGRAM, 0) < 0)) {
-        perror ("erro ao criar socket");
-        exit (0);
-    }
-
-    memset (&server_addr, 0, sizeof (server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr (IP_ADRESS);
-    server_addr.sin_port = htons (PORT);
-    
-    sendto (sockdg, message, strlen (message), 0, 
-           (struct sockaddr *) &server_addr, sizeof (server_addr));
-
-    close (sockdg);
-}    
-
-void receive_msg () {
-    int sockdg;
-    char buffer[BUFFER_SIZE];
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_len = sizeof (client_addr);
-
-    if ((sockdg = socket (AF_INET, SOCK_DGRAM, 0) < 0)) {
-        perror ("erro ao criar socket");
-        exit (0);
-    }
-
-    memset (&server_addr, 0, sizeof (server_addr));
-    memset (&client_addr, 0, sizeof (client_addr));
-
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htonl (INADDR_ANY);
-    server_addr.sin_port = htons (PORT);
-
-    if (bind 
-       (sockdg, (struct sockaddr *) &server_addr, sizeof (server_addr)) < 0) {
-           perror ("erro ao fazer bind");
-           exit (0);
-    }
-
-    if (recvfrom (sockdg, buffer, BUFFER_SIZE, 0, 
-       (struct sockaddr *) &client_addr, &client_len) < 0) {
-            perror ("erro ao receber mensagem");
-            exit (0);
-    }
-
-    printf ("# %s\n", buffer);
-
-    close (sockdg);
-}
+#define SERVER_IP "192.168.0.166"
+#define BUF_SIZE 1024
 
 int main (int argc, char **argv) {
-    int sockdg;
-    char message[BUFFER_SIZE];
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_len = sizeof (client_addr);
-
     if (strcmp (argv[1], "e") == 0) {
-        if ((sockdg = socket (AF_INET, SOCK_DGRAM, 0) < 0)) {
-            perror ("erro ao criar socket");
-            exit (0);
+        int sockfd;
+        struct sockaddr_in server_addr;
+        char buffer[BUF_SIZE];
+
+        // Criar socket datagrama
+        if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+            perror("Erro ao criar socket");
+            exit(EXIT_FAILURE);
         }
 
-        memset (&server_addr, 0, sizeof (server_addr));
+        // Configurar estrutura do servidor
+        memset(&server_addr, 0, sizeof(server_addr));
         server_addr.sin_family = AF_INET;
-        server_addr.sin_addr.s_addr = inet_addr (IP_ADRESS);
-        server_addr.sin_port = htons (PORT);
-        
-        while (1) {
-            printf ("# mensagem: ");
-            fgets(message, BUFFER_SIZE, stdin);
-            
-            sendto (sockdg, message, strlen (message), 0, 
-                   (struct sockaddr *) &server_addr, sizeof (server_addr));
+        server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+        server_addr.sin_port = htons(PORT);
 
-            memset (message, 0, BUFFER_SIZE);
+        while (1) {
+            printf("Digite uma mensagem para enviar ao servidor: ");
+            fgets(buffer, BUF_SIZE, stdin);
+
+            // Enviar mensagem ao servidor
+            sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *) &server_addr, sizeof(server_addr));
+
+            // Limpar buffer
+            memset(buffer, 0, BUF_SIZE);
         }
 
-        close (sockdg);
+        // Fechar socket
+        close(sockfd);
+
+        return 0;
     }
 
     else if (strcmp (argv[1], "r") == 0) {
-        if ((sockdg = socket (AF_INET, SOCK_DGRAM, 0) < 0)) {
-            perror ("erro ao criar socket");
-            exit (0);
+        int sockfd;
+        struct sockaddr_in server_addr, client_addr;
+        socklen_t client_len = sizeof(client_addr);
+        char buffer[BUF_SIZE];
+
+        // Criar socket datagrama
+        if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+            perror("Erro ao criar socket");
+            exit(EXIT_FAILURE);
         }
 
-        memset (&server_addr, 0, sizeof (server_addr));
+        // Configurar estrutura do servidor
+        memset(&server_addr, 0, sizeof(server_addr));
         server_addr.sin_family = AF_INET;
-        server_addr.sin_addr.s_addr = htonl (INADDR_ANY);
-        server_addr.sin_port = htons (PORT);
+        server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        server_addr.sin_port = htons(PORT);
 
-        if (bind (sockdg, (struct sockaddr *) &server_addr, 
-            sizeof (server_addr)) < 0) {
-                perror ("erro ao fazer bind");
-                exit (0);
+        // Associar socket a um endereço/porta
+        if (bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+            perror("Erro ao associar endereço ao socket");
+            exit(EXIT_FAILURE);
         }
+
+        printf("Servidor esperando por mensagens...\n");
 
         while (1) {
-            if (recvfrom (sockdg, message, BUFFER_SIZE, 0, 
-               (struct sockaddr *) &client_addr, &client_len) < 0) {
-                    perror ("erro ao receber mensagem");
-                    exit (0);
+            // Receber mensagem do cliente
+            if (recvfrom(sockfd, buffer, BUF_SIZE, 0, (struct sockaddr *) &client_addr, &client_len) < 0) {
+                perror("Erro ao receber mensagem");
+                exit(EXIT_FAILURE);
             }
 
-            printf ("# %s\n", message);
+            printf("Mensagem recebida do cliente: %s\n", buffer);
         }
+
+        // Fechar socket
+        close(sockfd);
+
+        return 0;
     }
 
     return 0;
