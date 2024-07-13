ab2159f97b34ca78d6cbb5640a5dabc3d6318d8f
Rafael Santos <rafa1405rafael@gmail.com>
2024-07-13 20:52:41 -0300

    .

--- t2/jogo.c
+++ t2/jogo.c
@@ -8,84 +8,74 @@
 #include <arpa/inet.h>
 
 #define PORT 33000
-#define SERVER_IP "192.168.0.166"
-#define BUF_SIZE 1024
+#define IP_ADRESS "192.168.0.166"
+#define BUFFER_SIZE 1024
 
 int main (int argc, char **argv) {
     if (strcmp (argv[1], "e") == 0) {
         int sockfd;
         struct sockaddr_in server_addr;
-        char buffer[BUF_SIZE];
+        char mensagem[BUFFER_SIZE];
 
-        // Criar socket datagrama
-        if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-            perror("Erro ao criar socket");
-            exit(EXIT_FAILURE);
+        if ((sockfd = socket (AF_INET, SOCK_DGRAM, 0)) < 0) {
+            perror ("erro ao criar socket");
+            exit (0);
         }
 
-        // Configurar estrutura do servidor
-        memset(&server_addr, 0, sizeof(server_addr));
+        memset (&server_addr, 0, sizeof (server_addr));
         server_addr.sin_family = AF_INET;
-        server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
-        server_addr.sin_port = htons(PORT);
+        server_addr.sin_addr.s_addr = inet_addr (IP_ADRESS);
+        server_addr.sin_port = htons (PORT);
 
         while (1) {
-            printf("Digite uma mensagem para enviar ao servidor: ");
-            fgets(buffer, BUF_SIZE, stdin);
+            printf ("# mensagem: ");
+            fgets (mensagem, BUFFER_SIZE, stdin);
 
-            // Enviar mensagem ao servidor
-            sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *) &server_addr, sizeof(server_addr));
+            sendto (sockfd, mensagem, strlen (mensagem), 0, (struct sockaddr *) &server_addr, sizeof (server_addr));
 
-            // Limpar buffer
-            memset(buffer, 0, BUF_SIZE);
+            memset (mensagem, 0, BUFFER_SIZE);
         }
 
-        // Fechar socket
-        close(sockfd);
-
-        return 0;
+        close (sockfd);
     }
 
     else if (strcmp (argv[1], "r") == 0) {
-        int sockfd;
+        int sockfd, n;
         struct sockaddr_in server_addr, client_addr;
-        socklen_t client_len = sizeof(client_addr);
-        char buffer[BUF_SIZE];
+        socklen_t client_len = sizeof (client_addr);
+        char mensagem[BUFFER_SIZE];
 
-        // Criar socket datagrama
-        if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-            perror("Erro ao criar socket");
-            exit(EXIT_FAILURE);
+        if ((sockfd = socket (AF_INET, SOCK_DGRAM, 0)) < 0) {
+            perror ("erro ao criar socket");
+            exit (0);
         }
 
-        // Configurar estrutura do servidor
-        memset(&server_addr, 0, sizeof(server_addr));
+        memset (&server_addr, 0, sizeof (server_addr));
         server_addr.sin_family = AF_INET;
-        server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-        server_addr.sin_port = htons(PORT);
+        server_addr.sin_addr.s_addr = htonl (INADDR_ANY);
+        server_addr.sin_port = htons (PORT);
 
-        // Associar socket a um endereço/porta
-        if (bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
-            perror("Erro ao associar endereço ao socket");
-            exit(EXIT_FAILURE);
+        if (bind (sockfd, (struct sockaddr *) &server_addr, sizeof (server_addr)) < 0) {
+            perror("erro ao realizar a bind");
+            exit(0);
         }
 
-        printf("Servidor esperando por mensagens...\n");
+        printf ("# esperando...\n");
 
         while (1) {
-            // Receber mensagem do cliente
-            if (recvfrom(sockfd, buffer, BUF_SIZE, 0, (struct sockaddr *) &client_addr, &client_len) < 0) {
-                perror("Erro ao receber mensagem");
-                exit(EXIT_FAILURE);
+            n = recvfrom (sockfd, mensagem, BUFFER_SIZE, 0, (struct sockaddr *) &client_addr, &client_len);
+
+            if (n < 0) {
+                perror ("erro ao receber mensagem");
+                exit (0);
             }
 
-            printf("Mensagem recebida do cliente: %s\n", buffer);
+            mensagem[n] = '\0';
+
+            printf ("%s", mensagem);
         }
 
-        // Fechar socket
-        close(sockfd);
-
-        return 0;
+        close (sockfd);
     }
 
     return 0;
